4707de95d620098c9b8666faa85e464b72fbfbc3
JY <kims2606@gmail.com>
2020-07-30 12:51:56 +0900

    DoIt 40P. 연습문제 1_15.

--- src/doIt/ch01/practice/Q_01_15.java
+++ src/doIt/ch01/practice/Q_01_15.java
@@ -16,6 +16,12 @@ public class Q_01_15 {
         System.out.println("\n2.왼쪽 위가 직각인 이등변 삼각형.");
         triangleLU(5);
 
+        System.out.println("\n3.오른쪽 위가 직각인 이등변 삼각형.");
+        triangleRU(5);
+
+        System.out.println("\n4.오른쪽 아래가 직각인 이등변 삼각형.");
+        triangleRB(5);
+
     }
 
     static void triangleLB(int n) {
@@ -36,4 +42,63 @@ public class Q_01_15 {
         }
     }
 
+    static void triangleRU(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(j >= i ? "*" : " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void triangleRB(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(j >= (n-1)-i ? "*" : " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //------------------- 아래는 답안지의 답안 ---------------------------------
+
+    /*// 왼쪽 아래가 직각인 이등변삼각형을 출력
+    static void triangleLB(int n) {
+        for (int i = 1; i <= n; i++) { 				// i행 (i = 1, 2, … ,n)
+            for (int j = 1; j <= i; j++) 			// i개의 '*'를 나타냄
+                System.out.print('*');
+            System.out.println(); 					// 개행(줄변환)
+        }
+    }
+
+    // 왼쪽 위가 직각인 이등변삼각형을 출력
+    static void triangleLU(int n) {
+        for (int i = 1; i <= n; i++) { 				// i행 (i = 1, 2, … ,n)
+            for (int j = 1; j <= n - i + 1; j++) 	// n-i+1개의 '*'를 나타냄
+                System.out.print('*');
+            System.out.println(); 					// 개행(줄변환)
+        }
+    }
+
+    // 오른쪽 위가 직각인 이등변삼각형을 출력
+    static void triangleRU(int n) {
+        for (int i = 1; i <= n; i++) { 				// i행 (i = 1, 2, … ,n)
+            for (int j = 1; j <= i - 1; j++) 		// i-1개의 ' '를 나타냄
+                System.out.print(' ');
+            for (int j = 1; j <= n - i + 1; j++) 	// n-i+1개의 '*'를 나타냄
+                System.out.print('*');
+            System.out.println();					// 개행(줄변환)
+        }
+    }
+
+    // 오른쪽 아래가 직각인 이등변삼각형을 출력
+    static void triangleRB(int n) {
+        for (int i = 1; i <= n; i++) { 				// i행 (i = 1, 2, … ,ln)
+            for (int j = 1; j <= n - i; j++) 		// n-i개의 ' '를 나타냄
+                System.out.print(' ');
+            for (int j = 1; j <= i; j++) 			// i개의 '*'를 나타냄
+                System.out.print('*');
+            System.out.println(); 					// 개행(줄변환)
+        }
+    }*/
 }
