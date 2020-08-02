f6be429ece5ffd7fcd386977e533b510b756e24e
JY <shutingz@naver.com>
2020-08-02 22:38:14 +0900

    DoIt 47P. 실습 2_2. int 배열 초기화

---
+++ src/doIt/ch02/IntArrayInit_02_02.java
@@ -0,0 +1,11 @@
+package doIt.ch02;
+
+public class IntArrayInit_02_02 {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
