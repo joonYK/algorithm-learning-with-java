1d1d3b73c9931197bf5ac099505c5ac939e09e14
JY <kims2606@gmail.com>
2020-08-14 12:34:08 +0900

    DoIt 80P. 실습 2_12. 2차원 배열

---
+++ src/doIt/ch02/Int2DArray.java
@@ -0,0 +1,21 @@
+package doIt.ch02;
+
+/**
+ * 2행 4열의 2차원 배열
+ */
+public class Int2DArray {
+    public static void main(String[] args) {
+        int[][] x = new int[2][4];
+
+        x[0][1] = 37;
+        x[0][3] = 54;
+        x[1][2] = 65;
+
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println("X[" + i + "][" + j + "] = " + x[i][j]);
+            }
+        }
+
+    }
+}
