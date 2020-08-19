ee7948676605738cbed977ab70db60e4b9e58288
JY <kims2606@gmail.com>
2020-08-19 17:43:26 +0900

    DoIt 102~103P. 실습 3_2. 선형검색

---
+++ src/doIt/ch03/SeqSearch_03_02.java
@@ -0,0 +1,41 @@
+package doIt.ch03;
+
+import java.util.Scanner;
+
+/**
+ * 선형 검색 (for문 사용)
+ */
+public class SeqSearch_03_02 {
+
+    //요솟수가 n인 배열 a에서 key와 같은 요소를 선형 검색
+    static int seqSearch(int[] a, int n, int key) {
+        for (int i = 0; i < n; i++) {
+            if (a[i] == key)
+                return i;   //검색 성공으로 인덱스 반환
+        }
+        return -1;  //검색 실패로 -1 반환
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("X[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        System.out.print("검색할 값 : ");
+        int ky = stdIn.nextInt();
+
+        int idx = seqSearch(x, num, ky);    //배열 x에서 키 값이 ky인 요소를 검색
+
+        if (idx == -1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky+"은(는) X[" + idx + "]에 있습니다.");
+    }
+}
