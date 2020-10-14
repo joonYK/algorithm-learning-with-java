94f9ebedd0a86ba4f7256c8912b5fe66308414a2
JY <kims2606@gmail.com>
2020-10-14 09:53:52 +0900

    DoIt 293 P. 연습문제 8_1. 브루트 포스법 문자열 검색 과정 출력.

---
+++ src/doIt/ch08/practice/Q_08_01.java
@@ -0,0 +1,102 @@
+package doIt.ch08.practice;
+
+import java.util.Scanner;
+
+/**
+ * 브루트 포스법의 검색 과정을 출력
+ *
+ * 0 ABABCDEFGHA
+ *   +
+ *   ABC
+ *
+ *   ABABCDEFGHA
+ *    +
+ *   ABC
+ *
+ *   ABABCDEFGHA
+ *     |
+ *   ABC
+ *
+ * 1 ABABCDEFGHA
+ *    |
+ *    ABC
+ *
+ *   ...중략...
+ *
+ * 비교는 7회였습니다.
+ */
+public class Q_08_01 {
+    
+    //브루트-포스법으로 문자열을 검색하는 메서드
+    static int bfMatch(String txt, String pattern) {
+        //txt 커서
+        int pt = 0;
+        //pattern 커서
+        int pp = 0;
+        //검색 index
+        int index = 0;
+        //비교 횟수
+        int count = 0;
+
+
+        while (pt != txt.length() && pp != pattern.length()) {
+            if(pp == 0)
+                System.out.println(index + " " + txt);
+
+            if (txt.charAt(pt) == pattern.charAt(pp)) {
+                System.out.printf("%2c%2s\n", ' ', txt);
+                System.out.printf(String.format("%%%dc", 2 + index + pp) + "%s\n", ' ', "+");
+                System.out.printf(String.format("%%%dc", 2 + index) + "%s\n", ' ', pattern);
+
+                pt++;
+                pp++;
+            } else {
+                System.out.printf("%2c%2s\n", ' ', txt);
+                System.out.printf(String.format("%%%dc", 2 + index + pp) + "%s\n", ' ', "|");
+                System.out.printf(String.format("%%%dc", 2 + index) + "%s\n", ' ', pattern);
+
+                pt = pt - pp + 1;
+                pp = 0;
+                index++;
+            }
+
+            count++;
+            System.out.println();
+        }
+
+        System.out.printf("\n비교는 %d회였습니다.\n", count);
+
+        //검색 성공
+        if (pp == pattern.length())
+            return pt - pp;
+
+        //검색 실패
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("텍스트 : ");
+        String s1 = stdIn.next();
+
+        System.out.print("패턴 : ");
+        String s2 = stdIn.next();
+
+        int idx = bfMatch(s1, s2);
+
+        if (idx == -1)
+            System.out.println("텍스트에 패턴이 없습니다.");
+        else {
+            //일치하는 문자 바로 앞까지의 길이를 구함
+            int len = 0;
+            for (int i = 0; i < idx; i++)
+               len += s1.substring(i, i + 1).getBytes().length;
+            len += s2.length();
+
+            System.out.println((idx + 1) + "번째 문자부터 일치합니다.");
+            System.out.println("텍스트 : " + s1);
+            System.out.printf(String.format("패턴 : %%%ds\n", len), s2);
+        }
+    }
+}
