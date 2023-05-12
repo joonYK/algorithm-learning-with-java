ed73d621feb8472afff1304280a3b3ac27558f12
김준엽 <kims2606@gmail.com>
2023-05-12 21:13:58 +0900

    POCU 삽입 정렬2

--- src/pocu/sort/Insertion.java
+++ src/pocu/sort/Insertion.java
@@ -4,10 +4,7 @@ import java.util.Arrays;
 
 public class Insertion {
 
-    static void sort(int[] arr) {
-        if (arr.length == 0)
-            return;
-
+    static void sort1(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             while (i > 0 && arr[i] < arr[i - 1]) {
                 int temp = arr[i];
@@ -18,9 +15,22 @@ public class Insertion {
         }
     }
 
+    static void sort2(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+
+            while (i > 0 && temp < arr[i - 1]) {
+                arr[i] = arr[i - 1];
+                i--;
+            }
+
+            arr[i] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{5,3,11,7,6,1,10,42,10,20,15,34};
-        Insertion.sort(arr);
+        Insertion.sort2(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
